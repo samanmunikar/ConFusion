8a11584612bac441250dcae9074c4ba0818a6c7a
Saman <samanmunikar@gmail.com>
2019-04-09 23:22:13 +0545

    Components Part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+export class Dish {
+    //properties
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
