f8eae811c42da1a4d829891f98090035c44b8224
Saman <samanmunikar@gmail.com>
2019-04-14 15:56:11 +0545

    Single Page Application Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'about', component: AboutComponent},
+  { path: 'dishdetail/:id', component: DishdetailComponent},
   { path: 'contactus', component: ContactComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -10,11 +12,21 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
   
-  @Input() dish: Dish;
+  dish: Dish;
 
-  constructor() { }
+  constructor(
+    private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location,
+    ) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
